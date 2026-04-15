290da9671cf348c5dc78329d52535b433784bcad
sstraccialini <stra.samuele@gmail.com>
2026-04-15 18:37:02 +0200

    Updated code to extract features on a custom dataset rather than on Charades specifically

--- extract_features.py
+++ extract_features.py
@@ -29,21 +29,65 @@ import numpy as np
 
 from pytorch_i3d import InceptionI3d
 
-from charades_dataset_full import Charades as Dataset
+import cv2
+import glob
 
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, root, mode, transforms=None, save_dir=''):
+        self.root = root
+        self.mode = mode
+        self.transforms = transforms
+        self.save_dir = save_dir
+        self.videos = [d for d in os.listdir(root) if os.path.isdir(os.path.join(root, d))]
 
-def run(max_steps=64e3, mode='rgb', root='/ssd2/charades/Charades_v1_rgb', split='charades/charades.json', batch_size=1, load_model='', save_dir=''):
+    def __len__(self):
+        return len(self.videos)
+
+    def __getitem__(self, idx):
+        vid = self.videos[idx]
+        if os.path.exists(os.path.join(self.save_dir, vid+'.npy')):
+            # return dummy data if already extracted
+            return torch.zeros(1), torch.zeros(1), vid
+            
+        vid_dir = os.path.join(self.root, vid)
+        frames_paths = sorted(glob.glob(os.path.join(vid_dir, '*.jpg')) + glob.glob(os.path.join(vid_dir, '*.png')))
+        frames = []
+        
+        for f in frames_paths:
+            img = cv2.imread(f)
+            if img is None: continue
+            if self.mode == 'rgb':
+                img = img[:, :, [2, 1, 0]]
+            else: # flow handling (assuming x and y are channels or greyscale)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                img = np.expand_dims(img, axis=2)
+                
+            w, h = img.shape[:2]
+            if w < 226 or h < 226:
+                d = 226.-min(w,h)
+                sc = 1+d/min(w,h)
+                img = cv2.resize(img, dsize=(0,0), fx=sc, fy=sc)
+            img = (img/255.)*2 - 1
+            frames.append(img)
+            
+        imgs = np.asarray(frames, dtype=np.float32)
+        if self.transforms:
+            imgs = self.transforms(imgs)
+            
+        # T x H x W x C -> C x T x H x W
+        imgs = torch.from_numpy(imgs.transpose([3,0,1,2]))
+        
+        return imgs, 0, vid
+
+def run(max_steps=64e3, mode='rgb', root='/ssd2/charades/Charades_v1_rgb', batch_size=1, load_model='', save_dir=''):
     # setup dataset
-    test_transforms = transforms.Compose([videotransforms.CenterCrop(224)])
+    test_transforms = transforms.Compose([videotransforms.CenterCrop(224)])     
 
-    dataset = Dataset(split, 'training', root, mode, test_transforms, num=-1, save_dir=save_dir)
+    dataset = CustomDataset(root, mode, test_transforms, save_dir=save_dir)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
 
-    val_dataset = Dataset(split, 'testing', root, mode, test_transforms, num=-1, save_dir=save_dir)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)    
-
-    dataloaders = {'train': dataloader, 'val': val_dataloader}
-    datasets = {'train': dataset, 'val': val_dataset}
+    dataloaders = {'val': dataloader}
+    datasets = {'val': dataset}
 
     
     # setup the model
@@ -55,9 +99,9 @@ def run(max_steps=64e3, mode='rgb', root='/ssd2/charades/Charades_v1_rgb', split
     i3d.load_state_dict(torch.load(load_model))
     i3d.cuda()
 
-    for phase in ['train', 'val']:
+    for phase in ['val']:
         i3d.train(False)  # Set model to evaluate mode
-                
+
         tot_loss = 0.0
         tot_loc_loss = 0.0
         tot_cls_loss = 0.0
